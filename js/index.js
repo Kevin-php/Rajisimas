6226c9a96c09874efd359fae86820d127d065d64
Kevin <kevinyt.kf@gmail.com>
2022-03-30 03:10:23 -0600

    Rajisimas Final 30/03/2022

---
+++ js/index.js
@@ -0,0 +1,33 @@
+//barra saliente
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+    // document.getElementById("btn-up").style.visibility = "visible";
+  } else {
+    document.getElementById("navbar").style.top = "-100px";
+    // document.getElementById("btn-up").style.visibility = "hidden";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+let btn = document.getElementById("btn-up");
+btn.addEventListener("click", function(){
+  document.getElement.scrollTop=0;
+})
+
+window.addEventListener("scroll", function(){
+  if(document.documentElement.scrollTop > 0){
+    btn.style.display = "flex";
+  }else{
+    btn.style.display = "none";
+  }
+})
